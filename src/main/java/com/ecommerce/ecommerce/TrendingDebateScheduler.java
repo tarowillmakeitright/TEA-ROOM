ba0d1b6e30824ce25d9b3da6725e7073bfd92a1d
tarowillmakeitright <ryutaro.maegima@gmail.com>
2026-05-07 00:14:38 +0900

    Enable scheduled news generation

--- src/main/java/com/ecommerce/ecommerce/TrendingDebateScheduler.java
+++ src/main/java/com/ecommerce/ecommerce/TrendingDebateScheduler.java
@@ -1,10 +1,14 @@
 package com.ecommerce.ecommerce;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TrendingDebateScheduler {
+    private static final Logger log = LoggerFactory.getLogger(TrendingDebateScheduler.class);
+
     private final TrendingDebateService service;
 
     public TrendingDebateScheduler(TrendingDebateService service) {
@@ -13,6 +17,7 @@ public class TrendingDebateScheduler {
 
     @Scheduled(cron = "0 0 */4 * * *", zone = "Asia/Tokyo")
     public void runEveryFourHours() {
+        log.info("Running scheduled Coffeehouse news generation");
         service.runOnceNow();
     }
 }
